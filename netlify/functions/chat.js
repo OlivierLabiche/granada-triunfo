812dddd24ef1134931417a0da03fbf2f0c52125c
OlivierLabiche <capitaine.flane@gmail.com>
2026-01-31 15:12:40 +0100

    Update chat.js

--- netlify/functions/chat.js
+++ netlify/functions/chat.js
@@ -62,59 +62,61 @@ FICHE D'INFORMATIONS
 ========================
 
 ACCÈS:
-Adresse: Acera de San Ildefonso 26, 3ème étage porte droite
-Code boîte à clés: 9119 (bas gauche de la porte)
+Adresse: Acera de San Ildefonso 26, 3ème étage porte droite.
+Code boîte à clés: 9119 (bas gauche de la porte).
 Porte immeuble toujours ouverte. Pas d'ascenseur.
 
 WIFI:
 Réseau: MOVISTAR_9EEO
 Mot de passe: Art&Deco2026
 
-CHAUFFAGE (RADIATEURS ÉLECTRIQUES):
-- Pour activer les radiateurs: mettre le fusible (cercle rouge) en position HAUTE sur le compteur à gauche de l'entrée
-- Les radiateurs sont dans tout l'appartement
-- Salle de bain: chauffage d'appoint disponible en plus
-- ATTENTION: le chauffage N'EST PAS la clim, ce sont les radiateurs électriques
+CHAUFFAGE:
+Radiateurs: fusible (cercle rouge) en position haute sur compteur à gauche de l'entrée.
+⚠️ Ne pas confondre avec la clim (chambres uniquement) qui sert pour le froid ET le chaud.
+Salle de bain: chauffage d'appoint disponible.
 
-CLIMATISATION (CHAMBRES UNIQUEMENT - POUR LE FROID):
-- Disponible dans CHAQUE CHAMBRE uniquement, PAS dans le salon
-- Utiliser UNIQUEMENT la télécommande dédiée (une par chambre)
-- PAS d'interrupteur mural pour la clim
-- La clim sert pour le FROID, pas pour chauffer
-
-VENTILATEUR DE PLAFOND (SALON UNIQUEMENT):
-- 1) D'abord allumer l'interrupteur mural à GAUCHE de la porte du salon
-- 2) Puis utiliser la télécommande Sulion (vitesse 1-6)
-- L'interrupteur mural est UNIQUEMENT pour le ventilateur du salon
-- Cet interrupteur N'A RIEN À VOIR avec la clim des chambres
+CLIM:
+Clim dans chaque chambre (pas salon), télécommande dans chaque chambre.
+La clim sert pour le froid ET le chaud.
+Ventilateur salon: 1) interrupteur mural à gauche 2) télécommande Sulion.
 
 CUISINE:
 Plaques Bosch: On/Off, sélectionner plaque, +/-
-Nespresso: eau derrière, capsules dans placard
-Hotte: brancher la prise pour activer
-Tri: bleu=papier, jaune=plastique, vert=verre, gris=reste. Conteneurs en face.
+Nespresso: eau derrière, capsules dans placard.
+Hotte: brancher la prise pour activer.
+Tri: bleu=papier, jaune=plastique, vert=verre, gris=reste. Conteneurs en face de l'immeuble en sortant.
 
 SALLE DE BAIN:
 Lave-linge dans buanderie après cuisine. Lessive dans commode.
 Bouteille de gaz: 3 bonbonnes de rechange.
 
+DRAPS CANAPÉ-LIT:
+Les draps, oreillers et couettes pour le canapé-lit se trouvent sous la méridienne (chaise longue).
+
 SALON:
 TV Xiaomi avec Netflix, Prime, YouTube.
 
 RESTAURANTS:
-ATIPICO (rez-de-chaussée): petit-déj sous les orangers, fermé dimanche.
+ATIPICO (rez-de-chaussée de l'immeuble): petit-déj sur la terrasse extérieure sous les orangers, fermé dimanche.
 LOS DIAMANTES (Plaza Nueva): meilleures tapas poisson, y aller 13h ou 20h.
-TORQUATO (Calle Pagés): préféré de Marie, friture, gaspacho.
+TORCUATO (Calle Pagés): préféré de Marie, friture, gazpacho. Terrasse dans l'Albaicín.
 LA TRASTIENDA (Plaza Cuchilleros): salle cachée, vin, fromage.
-PAPRIKA (Puerta Elvira): végétarien.
+PAPRIKA (Cuesta de Abarqueros 3): végétarien, à 5 mn à pied. Ouvert tous les jours 13h00-16h30 et 20h00-23h30. Réservation conseillée.
 HICURI (Realejo): 100% végétarien.
 Astuce: tapas GRATUITES avec chaque boisson à Grenade !
 
+PAELLA:
+Pour déguster une paella à Grenade: Maese Pío Arrocería (20-30€), Placeta del Pulgar. Ou Restaurante Los Manueles (10-20€), Calle Reyes Católicos. Temps de préparation 20-30 min, réserver et commander à l'avance.
+
 COURSES:
 AL SUR DE GRANADA (200m): épicerie fine, pain.
-HORNO DEL PROGRESO: boulangerie.
-TETERÍA ORIENTE (Puerta Elvira): thé, pâtisseries arabes.
-MERCADONA (Calle Ancha Capuchinos): 9h-21h, fermé dimanche.
+HORNO DEL PROGRESO (Calle Real de Cartuja 13): boulangerie.
+TETERÍA ORIENTE (Acera de la Merced 4, près Puerta Elvira): thé, pâtisseries arabes.
+MERCADONA (Calle Ancha de Capuchinos): supermarché, 9h-21h, fermé dimanche. À 500m.
+Petite épicerie à 5 mn: Calle Real de Cartuja 55.
+
+CALLE DE LAS TETERÍAS:
+La rue Calderería Nueva est la célèbre CALLE DE LAS TETERÍAS: nombreux salons de thé, boutiques de douceurs et artisanat arabe.
 
 BAIGNADE:
 Rivière gratuite: bout du Paseo de los Tristes, sous le pont.
@@ -127,27 +129,45 @@ HAMMAM AL ÁNDALUS (Plaza Santa Ana): bains arabes, réserver.
 ALBAICÍN: ruelles blanches.
 SACROMONTE: grottes, flamenco.
 
+ALHAMBRA SANS BILLETS:
+Si pas de billets pour l'Alhambra: monter par la Cuesta de Gómerez, passer par les remparts jusqu'au Palais de Charles Quint, puis descendre par la Cuesta de los Chinos, un chemin peu fréquenté dans la nature qui débute après le restaurant Las Mimbres. Chaussures confortables conseillées.
+
+BUS ALBAICÍN:
+Pour monter dans l'Albaicín depuis le centre, prendre les minibus urbains (petits bus rouges). La ligne C31 monte au Mirador de San Nicolás. Conseil: monter en minibus et redescendre dans les ruelles à pied !
+
 MIRADORS:
-San Nicolás: coucher de soleil, vue Alhambra.
-San Miguel Alto: plus calme, vue 360°.
+San Nicolás: coucher de soleil, vue Alhambra. Accès à pied depuis l'appartement en 15-20 minutes (pentes). Excellent pour le coucher de soleil.
+San Miguel Alto: plus calme, vue 360°. Accès à pied (30 mn, très pentu) ou bus N9 (arrêt Constitución2-Triunfo).
 
 FLAMENCO:
-PEÑA LA PLATERÍA: authentique, pas cher.
+PEÑA LA PLATERÍA: Placeta de Toqueros 7. Authentique, pas cher. À 20 mn à pied ou minibus C31 jusqu'à Plaza Nueva. Programme: http://www.laplateria.org.es/. Réservation conseillée. Ouvert mercredi à dimanche. Goûtez aussi leurs tapas et profitez de la vue sur l'Alhambra !
 
 FAMILLE:
 Parc jeux: 100m à gauche en sortant.
 PARC DES SCIENCES: activité n°1, métro Alcázar del Genil.
 
 TRANSPORT:
-Taxi: Plaza Triunfo, tél +34 958 28 06 54
-Aéroport: Línea 245, arrêt Constitución, 3.10€
+Taxi: station Plaza Triunfo, à 3 mn à pied. Tél +34 958 28 06 54. On peut aussi arrêter un taxi dans la ville (lumière verte = disponible). Application Pidetaxi disponible.
+Aéroport: navette Línea 245, arrêt Constitución, 3,10€. Plus d'infos: https://www.granadadirect.com/transporte/autobuses-aeropuerto-granada/. Aussi possible en taxi (30-40€).
+
+OÙ SE GARER:
+La rue de l'appartement est en zone bleue (horodateur), comme les rues adjacentes. Parking APK2 Triunfo à 5 mn à pied (réservation possible, utiliser l'application pour tarif réduit). Stationnement gratuit: zone CALLEJÓN DE LEBRIJA, à 15 mn à pied de l'appartement.
+
+SIERRA NEVADA:
+À 45 mn en voiture. Bus ALSA depuis la gare routière. Pour skier: forfait, météo et location de matériel sur sierranevada.es. Section "Été" du site: activités, randonnées, nature.
+
+THÉÂTRE:
+Théâtres célèbres: Isabel la Católica et Teatro Alhambra. Petite salle à 5 mn à pied: La Estupenda. Programmation: https://espaciolaestupenda.com.
+
+FEDERICO GARCÍA LORCA:
+L'un des plus grands poètes et dramaturges espagnols du XXe siècle, inspiré par la culture andalouse. Arrêté et assassiné en 1936 au début de la guerre civile. Centre culturel García Lorca: Plaza de la Romanilla (expositions, théâtre). Maison natale: Huerta de San Vicente, dans le parc García Lorca. Bus U3 depuis arrêt Severo Ochoa.
 
 URGENCES:
 Général: 112
-Centre médical: Gran Capitán 10, tél +34 958 022 600
+Centre médical: Gran Capitán 10, tél +34 958 022 600. Attention: urgences uniquement à partir de 15h.
 
 DÉPART:
-Avant 12h. Clés dans boîtier. Éteindre tout. Poubelles en face.
+Avant 12h. Clés dans boîtier. Éteindre tout. Poubelles dans conteneurs en face de l'immeuble.
 `;
 
     const response = await fetch("https://api.anthropic.com/v1/messages", {
